ae2e6b8a192bbd26d506e35dc7eabd597272168a
Taras Haidar <gajdartaras@gmail.com>
2019-05-15 16:11:23 +0200

    Updates

---
+++ precache-manifest.77e04305bbb4112f8e4b5f857280dfa1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1147f98f2874e3d0ed16b109ac90666c",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "94efe036953c3ec25094",
+    "url": "/react-project/static/js/2.052c9b92.chunk.js"
+  },
+  {
+    "revision": "318c509aa09f31a76ab2",
+    "url": "/react-project/static/js/main.f24180ad.chunk.js"
+  },
+  {
+    "revision": "c38e84bee75be2db3df6",
+    "url": "/react-project/static/js/runtime~main.270f79fb.js"
+  }
+]);
